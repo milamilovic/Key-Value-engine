a1d4c7698dc58ef0e3eb099383e1e4c78efd6e74
milamilovic <milamilovic03@gmail.com>
2023-02-01 15:07:47 +0100

    working on main

--- Spojeno/Main.go
+++ Spojeno/Main.go
@@ -2,23 +2,53 @@ package main
 
 import (
 	"Strukture/BloomFilter"
+	"Strukture/Cache"
 	"Strukture/CountMinSketch"
 	"Strukture/MemTable"
 	"Strukture/HyperLogLog"
+	"Strukture/Wal"
 	"fmt"
+	"os"
 )
 
 type Engine struct {
-	bloom    BloomFilter.BloomFilter
-	memtable MemTable.memTable
+	bloom         BloomFilter.BloomFilter
+	memtable      MemTable.MemTable
+	cache         Cache.Cache
+	wal           Wal.Wal
+	konfiguracije map[string]int
 }
 
-func init() {
+func default_konfig(engine *Engine) {
+	engine.konfiguracije["memtable_max_velicina"] = 15
+	engine.konfiguracije["cache_size"] = 10
+	engine.konfiguracije["sst_level"] = 3
+	engine.konfiguracije["sst_index"] = 4
+	engine.konfiguracije["lsm_level"] = 4
+	engine.konfiguracije["wal_low_water_mark"] = 3
+	engine.konfiguracije["token_key"] = 99999999
+	engine.konfiguracije["token_maxtok"] = 5
+	engine.konfiguracije["token_interval"] = 60
+}
 
+func initialize() *Engine {
+	engine := Engine{}
+	engine.konfiguracije = make(map[string]int)
+	file, err := os.ReadFile("Data/Konfiguracije/konfiguracije.txt")
+	if err != nil {
+		default_konfig(&engine)
+	} else {
+		fmt.Println(string(file))
+	}
+	engine.bloom = BloomFilter.New_bloom(engine.konfiguracije["memtable_max_velicina"], 0.1)
+	engine.memtable = MemTable.CreateMemTable(engine.konfiguracije["memtable_max_velicina"], engine.konfiguracije["memtable_max_velicina"])
+	engine.wal = Wal.NapraviWal("Spojeno\\Data\\Wal", engine.konfiguracije["wal_low_water_mark"])
+	return &engine
 }
 
 func main() {
-
+	engine := initialize()
+	fmt.Println(engine)
 }
 
 func makeCms() {
@@ -27,6 +57,6 @@ func makeCms() {
 }
 
 func makeHll() {
-	hll := HyperLogLog.makeHyperLogLog(8)
+	hll := HyperLogLog.MakeHyperLogLog(8)
 	fmt.Println(hll)
 }
