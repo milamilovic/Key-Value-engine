bc7bbd4438ec00c7469d9f67df9db5019e50ccc2
matejicdunja <dunja2002matejic@gmail.com>
2023-02-02 14:01:39 +0100

    brisanje?

--- Spojeno/brisanje.go
+++ Spojeno/brisanje.go
@@ -1,5 +1,16 @@
 package brisanje
 
-func obrisi(key string){
+import (
+	"Strukture/Cache"
+	"Strukture/MemTable"
+)
 
+func obrisi(key string, mt MemTable.MemTable, c Cache.Cache) bool {
+	// logicko brisanje
+	obrisan := mt.BrisiElement(key)
+	if obrisan {
+		c.ObrisiIzCache(key)
+		return true
+	}
+	return false
 }
