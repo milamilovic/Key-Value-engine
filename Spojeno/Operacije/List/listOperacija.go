a1e9795583dd7aae38ee0773b0445ac289401a85
matejicdunja <dunja2002matejic@gmail.com>
2023-02-04 22:55:18 +0100

    dalje list operacija

--- Spojeno/Operacije/List/listOperacija.go
+++ Spojeno/Operacije/List/listOperacija.go
@@ -1,5 +1,17 @@
 package List
 
+import (
+	"fmt"
+	"strings"
+)
+
 func List(podstring string, velicina int, broj int, kljucevi []string) [][]byte {
+	var potrebni_kljucevi []string
+	for kljuc := range kljucevi {
+		if strings.HasPrefix(kljucevi[kljuc], podstring) {
+			potrebni_kljucevi = append(potrebni_kljucevi, kljucevi[kljuc])
+		}
+	}
+	fmt.Println(potrebni_kljucevi)
 	return make([][]byte, 0)
 }
